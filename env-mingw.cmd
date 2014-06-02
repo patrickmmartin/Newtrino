3203edfa2b3a6300692592ecefb054b18eb47039
patrickmmartin@gmail.com <patrickmmartin@gmail.com>
2014-06-02 21:44:02 +0100

    updating path and typo

--- env-mingw.cmd
+++ env-mingw.cmd
@@ -1,4 +1,8 @@
-@echo setting Phython path
+@echo setting Python path c:\Python27
 @path c:\Python27\;%PATH%
+@echo setting emacs path C:\Program Files (x86)\emacs-22.3\bin\
+@path C:\Program Files (x86)\emacs-22.3\bin\;%PATH%
+@echo setting Graphviz path C:\Program Files (x86)\Graphviz2.36\bin
+@path C:\Program Files (x86)\Graphviz2.36\bin\;%PATH%
 @echo with MinGW, you can try this make version:
 @echo c:\MinGW\bin\mingw32-make.exe
