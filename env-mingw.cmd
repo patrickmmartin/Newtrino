33c8c5b26ef40b957458d2bedffa6fbd24778841
Patrick Martin <patrickmmartin@gmail.com>
2014-06-08 17:02:35 +0100

    more recent version of emacs for Windows env.

--- env-mingw.cmd
+++ env-mingw.cmd
@@ -1,7 +1,7 @@
 @echo setting Python path c:\Python27
 @path c:\Python27\;%PATH%
-@echo setting emacs path C:\Program Files (x86)\emacs-22.3\bin\
-@path C:\Program Files (x86)\emacs-22.3\bin\;%PATH%
+@echo setting emacs path C:\Program Files (x86)\emacs-24.3\bin\
+@path C:\Program Files (x86)\emacs-24.3\bin\;%PATH%
 @echo setting Graphviz path C:\Program Files (x86)\Graphviz2.36\bin
 @path C:\Program Files (x86)\Graphviz2.36\bin\;%PATH%
 @echo with MinGW, you can try this make version:
